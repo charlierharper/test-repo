92b42e36fe5109f55cc63109acc6b90db390b7f8
Git Actions - Linters <charlierharper@users.noreply.github.com>
2022-04-04 19:23:17 +0000

    isort, autopep8 fixes

--- bad-code.py
+++ bad-code.py
@@ -1,11 +1,14 @@
-import json, time, matplotlib
-import os, pandas
+import json
+import os
+import time
+
+import matplotlib
+import pandas
 import scipy
 
 
-def Func2(arg1: int =
-          None, arg_2=" test"):
+def Func2(arg1: int = None, arg_2=" test"):
     print("test   ")
 
 
-Func2(100.0, arg_2=      "test")
+Func2(100.0, arg_2="test")
