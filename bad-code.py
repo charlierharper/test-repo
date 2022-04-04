8ee5cad8fb93beac0d889e48a86582f4aec2024c
Charlie Harper <charlie.harper@flywheeldigital.com>
2022-04-04 12:51:45 -0400

    bad pyuthon

--- bad-code.py
+++ bad-code.py
@@ -5,8 +5,17 @@ import pandas
 import scipy
 
 
-def Func2(arg1: int = None, arg_2=" test"):
-    print("test ")
+def Func2(arg1:   int = None, arg_2   =" test"):
+    print ( "test ")
 
 
-Func2(100.0, arg_2="test")
+
+
+
+
+
+Func2(    100.0, arg_2 ="test")
+
+
+
+
