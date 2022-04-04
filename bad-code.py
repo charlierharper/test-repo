49b9276f9eaf74c7dfa6aac788ee52606e71cdbe
Charlie Harper <charlie.harper@flywheeldigital.com>
2022-04-04 15:20:18 -0400

    test

--- bad-code.py
+++ bad-code.py
@@ -1,12 +1,11 @@
-import json
-import os
-
-import pandas
+import json, time, matplotlib
+import os, pandas
 import scipy
 
 
-def Func2(arg1: int = None, arg_2=" test"):
-    print("test ")
+def Func2(arg1: int =
+          None, arg_2=" test"):
+    print("test   ")
 
 
-Func2(100.0, arg_2="test")
+Func2(100.0, arg_2=      "test")
